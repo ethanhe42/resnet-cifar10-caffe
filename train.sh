25ec7679c974ae4bd876f32ad816d184a740ec72
Yihui He 何宜晖 <yihuihe@foxmail.com>
2017-03-24 21:33:15 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -2,12 +2,14 @@
 set -x
 
 GPUs=$1
-NET=resnet-20
+# eg. 0,1,2,3
+NET=$2
+# eg. resnet-20
 EXTRA_ARGS=${array[@]:3:$len}
 EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
 
-solver=resnet-20/solver.prototxt
-LOG="resnet-20/logs/${NET}_${EXTRA_ARGS_SLUG}_`date +'%Y-%m-%d_%H-%M-%S'`.log"
+solver=${NET}/solver.prototxt
+LOG="${NET}/logs/${NET}_${EXTRA_ARGS_SLUG}_`date +'%Y-%m-%d_%H-%M-%S'`.log"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
