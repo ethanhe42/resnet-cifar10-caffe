e0fc22db161f430ce6f4f906657bf6f56aca1e42
Yihui He 何宜晖 <yihuihe@foxmail.com>
2017-03-25 12:26:09 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -4,6 +4,7 @@ set -x
 GPUs=$1
 # eg. 0,1,2,3
 NET=$2
+mkdir ${NET}/logs
 # eg. resnet-20
 EXTRA_ARGS=${array[@]:3:$len}
 EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
@@ -13,6 +14,7 @@ LOG="${NET}/logs/${NET}_${EXTRA_ARGS_SLUG}_`date +'%Y-%m-%d_%H-%M-%S'`.log"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
+# specify your caffe path here
 ~/caffe/build/tools/caffe train -gpu ${GPUs} \
     -solver ${solver} \
     -sighup_effect stop \
