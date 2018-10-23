93aefdbdda557188273e057932864e4f4829aaab
yihui-he <yihuihe@foxmail.com>
2018-10-23 17:20:40 -0400

    down

--- download_cifar.sh
+++ download_cifar.sh
@@ -2,6 +2,9 @@ file="cifar-10-batches-py"
 if [ ! -f $file.zip ]; then
     wget https://github.com/yihui-he/resnet-cifar10-caffe/releases/download/1.0/$file.zip
 fi
-tar -xzvf ../cifar-10-batches-py.zip
+mkdir $file
+cd $file
+tar -xzvf ../$file.zip
+cd ..
 rm $file.zip
 
